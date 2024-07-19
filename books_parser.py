41dfa4231c389949c2fdd33bff58238b2419aa96
ethomps33 <e.thomps33@gmail.com>
2024-07-19 14:00:42 -0700

    Created Method and route for calculating metrics

--- books_parser.py
+++ books_parser.py
@@ -6,8 +6,6 @@ import pandas as pd
 # Create app using Flask
 app = Flask(__name__)
 
-
-
 # Create Route for the Homepage
 @app.route('/')
 def homepage():
@@ -18,8 +16,8 @@ def homepage():
 # Route to View the Data from the books.csv file
 @app.route('/api', methods=['GET'])
 def api():
-    # file = request.file['books.csv']
-    data = pd.read_csv('books.csv')
+    file = request.files['books.csv']
+    data = pd.read_csv(file)
     return jsonify(data.to_dict())
 
 # Create Routes in the new app and set the HTTP function
@@ -27,26 +25,50 @@ def api():
 
 # Create Method that takes books.csv and returns a JSON Object
 def upload_file():
-    data = pd.read_csv('books.csv')
+    file = request.files['books.csv']
+    data = pd.read_csv(file)
     return jsonify(data.to_dict())
 
+# Method that takes the data from the CSV and a list of Categories with keywords and returns data grouped by the categories given
 def categorize_data(data, categories):
-    data = pd.read_csv('books.csv')
+    file = request.files['books.csv']
+    data = pd.read_csv(file)
     cat_data = {}
     for category, keywords in categories.items():
         cat_data[category] = data[data['description'].str.contains('|'.join(keywords), case=False, na=False)]
         return cat_data
 
+# Dictionary of different Categories for the data to be grouped by
 CATEGORIES = {
     'Murder Mystery': ['Suspicious','Unexpected', 'Courtroom', 'Murder', 'Psychopath']
 }
 
+# Route that returns the Categorized Data
 @app.route('/process', methods=['GET'])
 def process_data():
-    data = pd.read_csv('books.csv')
+    file = request.files['books.csv']
+    data = pd.read_csv(file)
     cat_data = categorize_data(data, CATEGORIES)
     return jsonify({cat: data.to_dict() for cat, data in cat_data.items()})
 
+# Method that Aggregates the Categorized Data to be used for Visulizations
+def aggragate_data(cat_data):
+    metrics = {}
+    for category, data in cat_data.items():
+        metrics[category] = {
+            total: data['title'].count(),
+            average_rating: data['average_rating'].mean(),
+            avg_pages: data['num_pages'].mean()
+        }
+        return metrics
+    
+@app.route('/metrics', method=['GET'])
+def calculate_metrics():
+    file = request.files['books.csv']
+    data = pd.read_csv(file)
+    cat_data = categorize_data(data, CATEGORIES)
+    metrics = aggragate_data(cat_data)
+    return jsonify(metrics)
 
 # Runs the app
 if __name__ == '__main__':
