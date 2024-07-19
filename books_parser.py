95d2f72cb55b4bd79cec76347b635c1d9389d5e0
ethomps33 <e.thomps33@gmail.com>
2024-07-19 16:47:00 -0700

    Corrected the aggrecate_data function. Added second category

--- books_parser.py
+++ books_parser.py
@@ -1,7 +1,7 @@
 # Import Necessary Libraries
 from flask import Flask, request, jsonify
 import pandas as pd
-
+from statistics import mode
 
 # Create app using Flask
 app = Flask(__name__)
@@ -16,8 +16,8 @@ def homepage():
 # Route to View the Data from the books.csv file
 @app.route('/api', methods=['GET'])
 def api():
-    file = request.files['books.csv']
-    data = pd.read_csv(file)
+    # file = request.files['books.csv']
+    data = pd.read_csv('books.csv')
     return jsonify(data.to_dict())
 
 # Create Routes in the new app and set the HTTP function
@@ -25,14 +25,14 @@ def api():
 
 # Create Method that takes books.csv and returns a JSON Object
 def upload_file():
-    file = request.files['books.csv']
-    data = pd.read_csv(file)
+    # file = request.files['books.csv']
+    data = pd.read_csv('books.csv')
     return jsonify(data.to_dict())
 
 # Method that takes the data from the CSV and a list of Categories with keywords and returns data grouped by the categories given
 def categorize_data(data, categories):
-    file = request.files['books.csv']
-    data = pd.read_csv(file)
+    # file = request.files['books.csv']
+    data = pd.read_csv('books.csv')
     cat_data = {}
     for category, keywords in categories.items():
         cat_data[category] = data[data['description'].str.contains('|'.join(keywords), case=False, na=False)]
@@ -41,31 +41,33 @@ def categorize_data(data, categories):
 # Dictionary of different Categories for the data to be grouped by
 CATEGORIES = {
     'Murder Mystery': ['Suspicious','Unexpected', 'Courtroom', 'Murder', 'Psychopath']
+    # 'Science Fiction': ['Aliens', 'Space', 'Cloning']
 }
 
 # Route that returns the Categorized Data
 @app.route('/process', methods=['GET'])
 def process_data():
-    file = request.files['books.csv']
-    data = pd.read_csv(file)
+    # file = request.files['books.csv']
+    data = pd.read_csv('books.csv')
     cat_data = categorize_data(data, CATEGORIES)
     return jsonify({cat: data.to_dict() for cat, data in cat_data.items()})
 
 # Method that Aggregates the Categorized Data to be used for Visulizations
 def aggragate_data(cat_data):
     metrics = {}
-    for category, data in cat_data.items():
-        metrics[category] = {
-            total: data['title'].count(),
-            average_rating: data['average_rating'].mean(),
-            avg_pages: data['num_pages'].mean()
+    for cat, data in cat_data.items():
+        metrics[cat] = {
+            'total_titles': data['title'].count().astype('float'),
+            'average_category_rating': data['average_rating'].mean(),
+            'avg_category_pages': data['num_pages'].mean(),
+            'most_frequent_author': mode(data['authors'])
         }
         return metrics
     
-@app.route('/metrics', method=['GET'])
+@app.route('/metrics', methods=['GET'])
 def calculate_metrics():
-    file = request.files['books.csv']
-    data = pd.read_csv(file)
+    # file = request.files['books.csv']
+    data = pd.read_csv('books.csv')
     cat_data = categorize_data(data, CATEGORIES)
     metrics = aggragate_data(cat_data)
     return jsonify(metrics)
